d7649f8771e6317f823fd228a9d5122d44aef1c4
LudoDescamps <atthevilla@free.fr>
2018-01-13 20:52:31 +0100

    add settiges model and provider

---
+++ src/providers/settings-service/settings-service.ts
@@ -0,0 +1,29 @@
+import { Settings } from './../../app/models/Settings';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the SettingsServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SettingsServiceProvider {
+  settings: Settings = {
+    language: 'french'
+  }
+
+  constructor() {
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
+
+  getSettings(){
+    return this.settings;
+  }
+
+  changeSettings(settings: Settings){
+    localStorage.setItem('settings', JSON.stringify(settings));
+  }
+}
